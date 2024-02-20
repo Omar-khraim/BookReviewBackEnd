86bbd1969c2d9e20442e2984540b211af7bb89d9
Omar-khraim <oaskhraim@gmail.com>
2024-02-20 18:54:59 -0500

    first commit

---
+++ src/main/java/com/example/bookreviewbackend/BookReviewBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookreviewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReviewBackEndApplication.class, args);
+    }
+
+}
