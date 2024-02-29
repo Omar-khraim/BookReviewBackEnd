cce67e790e3f9808e34adf147324cf5091fb26ee
Omar-khraim <oaskhraim@gmail.com>
2024-02-29 16:40:20 -0500

    commit

--- src/main/java/com/example/bookreviewbackend/Repository/ReviewRepo.java
+++ src/main/java/com/example/bookreviewbackend/Repository/ReviewRepo.java
@@ -1,2 +1,9 @@
-package com.example.bookreviewbackend.Repository;public interface ReviewRepo {
+package com.example.bookreviewbackend.Repository;
+
+import com.example.bookreviewbackend.Enitity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepo extends JpaRepository<Review, Long> {
 }
