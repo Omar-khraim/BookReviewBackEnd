f66840f1b8badc0b305446d517a8d806e05a474d
Omar-khraim <oaskhraim@gmail.com>
2024-02-29 16:16:31 -0500

    add users entity

--- src/main/java/com/example/bookreviewbackend/Enitity/Users.java
+++ src/main/java/com/example/bookreviewbackend/Enitity/Users.java
@@ -1,2 +1,46 @@
-package com.example.bookreviewbackend.Enitity;public class Users {
+package com.example.bookreviewbackend.Enitity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(indexes = @Index(name = "user_email_index", columnList = "email"))
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created;
+
+
+    @Temporal(TemporalType.DATE)
+    @UpdateTimestamp
+    private LocalDate updated;
+
+
+
 }
