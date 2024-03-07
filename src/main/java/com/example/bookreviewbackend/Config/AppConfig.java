60bfb6bb9f384700761e172831d53173108a9096
Omar-khraim <oaskhraim@gmail.com>
2024-03-06 19:12:46 -0500

    finish up the services of reviews

--- src/main/java/com/example/bookreviewbackend/Config/AppConfig.java
+++ src/main/java/com/example/bookreviewbackend/Config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.example.bookreviewbackend.Config;public class AppConfig {
+package com.example.bookreviewbackend.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
